60b44b61131ab5b4bc129604dc9808103ebdc891
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-10-03 13:57:01 +0300

    more @PathVariables tests - regex

--- src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/ControllerWithPathVariables.kt
+++ src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/ControllerWithPathVariables.kt
@@ -2,8 +2,10 @@ package com.mytests.spring.kotlin.mvckotlintest0
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 
 /**
  ********************************
@@ -15,11 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/pathvariables/{class_level_var}")
 class ControllerWithPathVariables {
 
-    @RequestMapping("/{p0}")
-    fun test0(model: Model, @PathVariable("p0") p0:String, @PathVariable("class_level_var") clv:String ):String{
+
+    @GetMapping("/{p0:[a-zA-z]+}")
+    fun test0(model: Model, @PathVariable p0:String, @PathVariable("class_level_var") clv:String):String{
         model.asMap()["attr01"] = "attr01"
         model.addAttribute("attr02", p0)
         model.asMap()["attr03"] = clv
         return "test0"
+    }
+
+    @GetMapping("/{version:\\d\\.\\d\\.\\d}")
+    fun test00(@PathVariable version:String,  @PathVariable("class_level_var") clv:String):ModelAndView{
+        val modelAndView = ModelAndView("test00")
+        modelAndView.addObject("version", version)
+        return modelAndView
     }
 }
