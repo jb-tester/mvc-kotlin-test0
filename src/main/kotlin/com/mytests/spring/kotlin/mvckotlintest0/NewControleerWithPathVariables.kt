a7b1e5165713056810fd129947b226c435c947f5
irina <irina@me.com>
2019-02-12 18:48:44 +0300

    NewControleerWithPathVariables.kt: correct contant reference

--- src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControleerWithPathVariables.kt
+++ src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControleerWithPathVariables.kt
@@ -9,7 +9,7 @@ private const val MY_CONST1 = "constants"
 private const val MY_CONST2 = "test000"
 @Controller
 class NewControllerWithPathVariables {
-    @GetMapping("/$MY_CONST1/{$MY_CONST1}")
+    @GetMapping("/$MY_CONST1/{$MY_CONST2}")
     fun test000(model: Model, @PathVariable() test000:String):String{
         model.addAttribute("attr0004",test000)
         return "test000"
