fadde83854049f6e3c252404704df368b97bdf66
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-02-12 19:49:52 +0300

    NewControllerWithPathVariables.kt: use const in concatenation

--- src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControllerWithPathVariables.kt
+++ src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControllerWithPathVariables.kt
@@ -5,11 +5,11 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-private const val MY_CONST1 = "constants"
+private const val MY_CONST1 = "/constants"
 private const val MY_CONST2 = "test000"
 @Controller
 class NewControllerWithPathVariables {
-    @GetMapping("/$MY_CONST1/{$MY_CONST2}")
+    @GetMapping("$MY_CONST1"+"/{$MY_CONST2}")
     fun test000(model: Model, @PathVariable() test000:String):String{
         model.addAttribute("attr0004",test000)
         return "test000"
