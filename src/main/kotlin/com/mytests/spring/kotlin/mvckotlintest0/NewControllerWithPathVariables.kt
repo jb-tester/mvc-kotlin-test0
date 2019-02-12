ee40f7044723c0f77989a181984449fa7baed341
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-02-12 20:01:15 +0300

    formatting

--- src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControllerWithPathVariables.kt
+++ src/main/kotlin/com/mytests/spring/kotlin/mvckotlintest0/NewControllerWithPathVariables.kt
@@ -10,7 +10,6 @@ private const val MY_CONST2 = "test000"
 @Controller
 class NewControllerWithPathVariables {
 
-    
     @GetMapping("$MY_CONST1"+"/{$MY_CONST2}")
     fun test000(model: Model, @PathVariable() test000:String):String{
         model.addAttribute("attr0004",test000)
